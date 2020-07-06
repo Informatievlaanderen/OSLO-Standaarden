7aa3df0c330f77e97156593ef4030400a7a06fdd
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-07-06 15:46:33 +0200

    Remove dummy + added logging entry in config

--- scripts/checkout.sh
+++ scripts/checkout.sh
@@ -66,6 +66,7 @@ if cat "$ROOTDIR/changedstandards.json" | jq -e . >/dev/null 2>&1; then
     BASE_URL="https://github.com/ddvlanck/$THEME_NAME/raw/standaardenregister"
     jq --arg BASE_URL "$BASE_URL" '. |= . + {"baseURL" : $BASE_URL}' "$CONFIG" > "$CONFIG-extended.json"
 
+    echo "Done processing"
 
   done
 else
