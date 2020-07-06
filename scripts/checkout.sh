b93b66243a601eac547043f127c7418bc0e88e25
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-07-06 14:59:39 +0200

    Automated standards registers

---
+++ scripts/checkout.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+PUBCONFIG=$2
+ROOTDIR=$1
+
+echo "I'm currently here $PWD"
+
+# Determine the last changed files
+mkdir -p "$ROOTDIR"
+curl -o "$ROOTDIR/commit.json" https://github.com/Informatievlaanderen/OSLO-StandaardenregisterGenerated/blob/master/commit.json
+sleep 5s
+jq . "$ROOTDIR/commit.json"
+if [ $? -eq 0 ]; then
+  PREV_COMMIT=$(jq -r .commit "$ROOTDIR/commit.json")
+  changedFiles=$(git diff --name-only "$PREV_COMMIT")
+  if [ "$changedFiles" == "standaardenregister.json" ]
+  then
+    echo "One or more standards seem to be modified"
+    echo "These standards were modified:"
+    git show "$PREV_COMMIT:standaardenregister.json" > previous_version
+    jq -s '.[0] - .[1]' standaardenregister.json previous_version
+    jq -s '.[0] - .[1]' standaardenregister.json previous_version > "$ROOTDIR/changedstandards.json"
+    cat "$ROOTDIR/changedstandards.json"
+  else
+    echo "One or more files were changed, possibly a script, so a full rebuild is necessary"
+    cp "${PUBCONFIG}" "$ROOTDIR/changedstandards.json"
+  fi
+
+else
+  # No previous commit
+  # Assumes full rebuild of all standards
+  echo "No previous commit was made."
+  echo "Processing all standards in standaardenregister.json"
+  cp "$PUBCONFIG" "$ROOTDIR/changedstandards.json"
+fi
+
+#Process all standards that have been changed or were added
+echo "Start processing the standards"
+if cat "$ROOTDIR/changedstandards.json" | jq -e . >/dev/null 2>&1; then
+  # Only iterate over those that have a repository
+  for row in $(jq -r '.[] | select(.repository)  | @base64 ' "$ROOTDIR/changedstandards.json"); do
+    _jq() {
+      echo "${row}" | base64 --decode | jq -r "${1}"
+    }
+
+    # Get name of repository to create a directory with the same name
+    REPOSITORY=$(_jq '.repository')
+    STATUS=$(_jq '.status')
+    CONFIG=$(_jq '.configuration')
+    CONFIG_NAME=$(echo "$CONFIG" | cut -d "." -f 1)
+<<<<<<< HEAD
+=======
+    DESCRIPTION_NAME=$(_jq '.description')
+>>>>>>> 3e7c5a1... Added scripts and circle config
+
+    THEME_NAME=$(echo "$REPOSITORY" | cut -d '/' -f 5)
+
+    mkdir -p "$ROOTDIR/repositories/$THEME_NAME"
+
+    ### We convert the standards register from JSON to a simple text file
+<<<<<<< HEAD
+    echo "$THEME_NAME:$CONFIG:$STATUS" >> "$ROOTDIR/tmp-register.txt"
+=======
+    echo "$THEME_NAME:$CONFIG:$STATUS:$DESCRIPTION_NAME" >> "$ROOTDIR/tmp-register.txt"
+>>>>>>> 3e7c5a1... Added scripts and circle config
+
+    ### Cloning repository and checking out branch standaardenregister
+    git clone "$REPOSITORY" "$ROOTDIR/repositories/$THEME_NAME"
+    cd "$ROOTDIR/repositories/$THEME_NAME"
+    git checkout standaardenregister
+
+    # We extend the the configuration file for a specific file in a theme with the baseURL
+    BASE_URL="https://github.com/ddvlanck/$THEME_NAME/raw/standaardenregister"
+    jq --arg BASE_URL "$BASE_URL" '. |= . + {"baseURL" : $BASE_URL}' "$CONFIG" > "$CONFIG-extended.json"
+
+<<<<<<< HEAD
+    echo "Done processing"
+=======
+>>>>>>> 3e7c5a1... Added scripts and circle config
+
+  done
+else
+  echo "Problem processing following file: ${PUBCONFIG}"
+fi
---
+++ scripts/extract-descriptions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ROOTDIR=$1
+REPODIR=$ROOTDIR/repositories
+
+while IFS= read -r line
+do
+  REPO_NAME=$(echo "$line" | cut -d ":" -f 1)
+
+  cd "$REPODIR/$REPO_NAME"
+  CONFIG_NAME=$(echo "$line" | cut -d ":" -f 2 | cut -d "." -f 1)
+
+  DESCRIPTION_NAME=$(jq -r '.description_file' "$CONFIG_NAME.json")
+  DESCRIPTION_NAME_NO_EXTENSION=$(echo "$DESCRIPTION_NAME" | cut -d '.' -f 1)
+
+  echo "$CONFIG_NAME:$DESCRIPTION_NAME" >> "$ROOTDIR/description-paths.txt"
+
+done < "$ROOTDIR/tmp-register.txt"
