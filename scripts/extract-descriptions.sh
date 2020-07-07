5563dab8172e23414c44c13e7d13a95813d339de
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-07-07 11:38:57 +0200

    AA

--- scripts/extract-descriptions.sh
+++ scripts/extract-descriptions.sh
@@ -12,8 +12,7 @@ do
 
   DESCRIPTION_NAME=$(jq -r '.description_file' "$CONFIG_NAME.json")
   DESCRIPTION_NAME_NO_EXTENSION=$(echo "$DESCRIPTION_NAME" | cut -d '.' -f 1)
-  DESCRIPTION_PATH="descriptions/$DESCRIPTION_NAME"
 
-  echo "$CONFIG_NAME:$DESCRIPTION_PATH" >> "$ROOTDIR/description-paths.txt"
+  echo "$CONFIG_NAME:$DESCRIPTION_NAME" >> "$ROOTDIR/description-paths.txt"
 
 done < "$ROOTDIR/tmp-register.txt"
--- scripts/markdown-transformer.sh
+++ scripts/markdown-transformer.sh
@@ -2,7 +2,7 @@
 
 ROOTDIR=$1
 REPODIR=$ROOTDIR/repositories
-DESCRIPTION_PATHS="$ROOTDIR/description-paths.txt"
+DESCRIPTION_NAMES="$ROOTDIR/description-paths.txt"
 
 mkdir -p "$ROOTDIR/descriptions"
 
@@ -15,10 +15,10 @@ do
   cd "$REPODIR/$REPO_NAME"
 
   CONFIG_NAME=$(echo "$line" | cut -d ":" -f 2 | cut -d "." -f 1)
-  DESCRIPTION_PATH=$(grep "$CONFIG_NAME" $DESCRIPTION_PATHS | cut -d ":" -f 2)
-  DESCRIPTION_NAME_NO_EXTENSION=$(echo $DESCRIPTION_PATH | cut -d "." -f 1)
+  DESCRIPTION_NAME=$(grep "$CONFIG_NAME" *DESCRIPTION_NAMES | cut -d ":" -f 2)
+  DESCRIPTION_NAME_NO_EXTENSION=$(echo $DESCRIPTION_NAME | cut -d "." -f 1)
 
   if test -f "$DESCRIPTION_PATH" ; then
-    node /app/index.js -f "$DESCRIPTION_PATH" -o "$ROOTDIR/descriptions/$DESCRIPTION_NAME_NO_EXTENSION-description.html"
+    node /app/index.js -f "descriptions/$DESCRIPTION_NAME" -o "$ROOTDIR/descriptions/$DESCRIPTION_NAME_NO_EXTENSION-description.html"
   fi
 done < "$ROOTDIR/tmp-register.txt"
