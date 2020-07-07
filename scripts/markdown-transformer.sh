4c6a38ec2035c569a18fc1b349d0627f52da8c08
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-07-07 11:19:39 +0200

    Added new step

--- scripts/markdown-transformer.sh
+++ scripts/markdown-transformer.sh
@@ -2,6 +2,7 @@
 
 ROOTDIR=$1
 REPODIR=$ROOTDIR/repositories
+DESCRIPTION_PATHS="description-paths.txt"
 
 mkdir -p "$ROOTDIR/descriptions"
 
@@ -10,14 +11,14 @@ while IFS= read -r line
 do
   REPO_NAME=$(echo "$line" | cut -d ":" -f 1)
   #DESCRIPTION_NAME=$(echo "$line" | cut -d ":" -f 4)
-  #DESCRIPTION_PATH="beschrijvingen/$DESCRIPTION_NAME"
 
   cd "$REPODIR/$REPO_NAME"
 
   CONFIG_NAME=$(echo "$line" | cut -d ":" -f 2 | cut -d "." -f 1)
+  DESCRIPTION_PATH=$(grep "$CONFIG_NAME" $DESCRIPTION_PATHS | cut -d ":" -f 2)
 
-
-  if test -f "$DESCRIPTION_PATH" ; then
-    node /app/index.js -f "$DESCRIPTION_PATH" -o "$ROOTDIR/descriptions/$DESCRIPTION_NAME_NO_EXTENSION-description.html"
-  fi
+echo $DESCRIPTION_PATH
+  #if test -f "$DESCRIPTION_PATH" ; then
+  #  node /app/index.js -f "$DESCRIPTION_PATH" -o "$ROOTDIR/descriptions/$DESCRIPTION_NAME_NO_EXTENSION-description.html"
+  #fi
 done < "$ROOTDIR/tmp-register.txt"
