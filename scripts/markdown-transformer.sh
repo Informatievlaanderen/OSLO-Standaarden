406d38822eec31a63dcf15d2e5eaa408a47a5e46
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-07-07 11:46:20 +0200

    Fix typo

--- scripts/markdown-transformer.sh
+++ scripts/markdown-transformer.sh
@@ -15,8 +15,8 @@ do
   cd "$REPODIR/$REPO_NAME"
 
   CONFIG_NAME=$(echo "$line" | cut -d ":" -f 2 | cut -d "." -f 1)
-  DESCRIPTION_NAME=$(grep "$CONFIG_NAME" *DESCRIPTION_NAMES | cut -d ":" -f 2)
-  DESCRIPTION_NAME_NO_EXTENSION=$(echo $DESCRIPTION_NAME | cut -d "." -f 1)
+  DESCRIPTION_NAME=$(grep "$CONFIG_NAME" "$DESCRIPTION_NAMES" | cut -d ":" -f 2)
+  DESCRIPTION_NAME_NO_EXTENSION=$(echo "$DESCRIPTION_NAME" | cut -d "." -f 1)
 
   if test -f "$DESCRIPTION_PATH" ; then
     node /app/index.js -f "descriptions/$DESCRIPTION_NAME" -o "$ROOTDIR/descriptions/$DESCRIPTION_NAME_NO_EXTENSION-description.html"
