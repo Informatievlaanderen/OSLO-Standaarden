5dbfc12fadcb0416ac41ce8eb0db6bfdec4fcd77
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-07-07 11:05:59 +0200

    Added logging

--- scripts/extract-descriptions.sh
+++ scripts/extract-descriptions.sh
@@ -3,17 +3,23 @@
 ROOTDIR=$1
 REPODIR=$ROOTDIR/repositories
 
+mkdir -p "$ROOTDIR/descriptions"
+
+
 while IFS= read -r line
 do
   REPO_NAME=$(echo "$line" | cut -d ":" -f 1)
+
   cd "$REPODIR/$REPO_NAME"
-
   CONFIG_NAME=$(echo "$line" | cut -d ":" -f 2 | cut -d "." -f 1)
+  echo "$CONFIG_NAME"
+  echo "Listing all files in $REPO_NAME"
+  ls
 
-  DESCRIPTION_NAME=$(jq -r '.description_file' "$CONFIG_NAME")
-  DESCRIPTION_NAME_NO_EXTENSION=$(echo "$DESCRIPTION_NAME" | cut -d '.' -f 1)
-  DESCRIPTION_PATH="descriptions/$DESCRIPTION_NAME"
+  #DESCRIPTION_NAME=$(jq -r '.description_file' "$CONFIG_NAME")
+  #DESCRIPTION_NAME_NO_EXTENSION=$(echo "$DESCRIPTION_NAME" | cut -d '.' -f 1)
+  #DESCRIPTION_PATH="descriptions/$DESCRIPTION_NAME"
 
-  echo "$CONFIG_NAME:$DESCRIPTION_PATH" >> "$ROOTDIR/description-paths.txt"
+  #echo "$CONFIG_NAME:$DESCRIPTION_PATH" >> "$ROOTDIR/description-paths.txt"
 
 done < "$ROOTDIR/tmp-register.txt"
