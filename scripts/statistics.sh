ad9cbe2d481daf9f82755a00369e565737fdc7fa
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-09-23 16:36:47 +0200

    Added logging

--- scripts/statistics.sh
+++ scripts/statistics.sh
@@ -11,6 +11,7 @@ touch "$ROOTDIR/statistics_config.json"
 echo "[]" > "$ROOTDIR/statistics_config.json"
 
 if cat "$FILENAME" | jq -e . >/dev/null 2>&1; then
+  echo "OKE"
   for row in $(jq -r '.[] | select(.repository)  | @base64 ' "$FILENAME"); do
     _jq() {
       echo "${row}" | base64 --decode | jq -r "${1}"
