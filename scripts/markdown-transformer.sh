c3e61ffeb0177e06d7365580db159c4564cc0fda
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-09-23 10:09:52 +0200

    Updated grep with regular expression to make sure there's only one result

--- scripts/markdown-transformer.sh
+++ scripts/markdown-transformer.sh
@@ -14,15 +14,13 @@ do
   cd "$REPODIR/$REPO_NAME"
 
   CONFIG_NAME=$(echo "$line" | cut -d ":" -f 2 | cut -d "." -f 1)
-  DESCRIPTION_NAME=$(grep "$CONFIG_NAME" "$DESCRIPTION_NAMES" | cut -d ":" -f 2)
+  DESCRIPTION_NAME=$(grep -w ^"$CONFIG_NAME" "$DESCRIPTION_NAMES" | cut -d ":" -f 2)
 
   if [ -z "$DESCRIPTION_NAME" ]
   then
     echo "No description was provided for config file $CONFIG_NAME"
   else
     DESCRIPTION_NAME_NO_EXTENSION=$(echo "$DESCRIPTION_NAME" | cut -d "." -f 1)
-
-    echo "Executing script for file $DESCRIPTION_NAME retrieved from config $CONFIG_NAME"
 
     # Execute the transform script
     node /app/index.js -f "descriptions/$DESCRIPTION_NAME" -o "$ROOTDIR/descriptions/$DESCRIPTION_NAME_NO_EXTENSION-description.html"
