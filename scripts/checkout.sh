9b7c3177d4589740978b64ef168bec71fcfebcf1
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2022-01-14 14:11:02 +0100

    Update checkout

--- scripts/checkout.sh
+++ scripts/checkout.sh
@@ -61,6 +61,9 @@ if cat "$ROOTDIR/changedstandards.json" | jq -e . >/dev/null 2>&1; then
 
     ### We extend the the configuration file for a specific file in a theme with the baseURL
     BASE_URL="https://github.com/Informatievlaanderen/$THEME_NAME/raw/standaardenregister"
+    jq --arg BASE_URL "$BASE_URL" '. |= . + {"baseURL" : $BASE_URL}' "$CONFIG" > "$CONFIG-extended.json"
+
+    ## Nuxt and correct filename
     jq --arg BASE_URL "$BASE_URL" '. |= . + {"baseURL" : $BASE_URL}' "$CONFIG" > "$CONFIG_NAME-extended.json"
 
     echo "Done processing"
