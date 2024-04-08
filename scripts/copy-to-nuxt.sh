658003a84710ab7cca706c203ab059ef5b305897
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2024-04-08 11:43:06 +0200

    Udpated directory name and config name for `copy-to-nuxt` script

--- scripts/copy-to-nuxt.sh
+++ scripts/copy-to-nuxt.sh
@@ -2,15 +2,15 @@
 
 ROOTDIR=$1
 REPODIR=$ROOTDIR/repositories
-NUXTDIR=$ROOTDIR/nuxt
+NUXTDIR=$ROOTDIR/content/standaarden
 
 mkdir -p "$NUXTDIR"
 
-## Copy each configuration file and description file to nuxt folder
+## Copy each configuration file and description file to content/standaarden folder
 while IFS= read -r line
 do
   REPO_NAME=$(echo "$line" | cut -d ":" -f 1)
-  CONFIG_NAME="configuration"
+  CONFIG_NAME=$(echo "$line" | cut -d ":" -f 2 | cut -d "." -f 1)
 
   cd "$REPODIR/$REPO_NAME"
 
@@ -22,5 +22,7 @@ do
 
   mkdir -p "$NUXTDIR/$NORMALIZED_SPEC_NAME"
 
-  cp "$CONFIG_NAME.json" "descriptions/$DESCRIPTION_NAME" "$NUXTDIR/$NORMALIZED_SPEC_NAME"
+  ## Copy the generated configuration file and description file to the nuxt directory
+  cp "$CONFIG_NAME.json" "$NUXTDIR/$NORMALIZED_SPEC_NAME/configuration.json"
+  cp "descriptions/$DESCRIPTION_NAME" "$NUXTDIR/$NORMALIZED_SPEC_NAME/description.md"
 done < "$ROOTDIR/tmp-register.txt"
