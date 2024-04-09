05477afd1949052ea6ab614d1f78e2e305fa6e3e
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2024-04-09 13:42:58 +0200

    Also copy the /description file to output repository

--- scripts/copy-to-nuxt.sh
+++ scripts/copy-to-nuxt.sh
@@ -22,9 +22,11 @@ do
 
   ## this needs to change to .title if the new json structure is used
   SPEC_NAME=$(jq -r '.naam' "$CONFIG_NAME.json")
+  ## this needs to change to .descriptionFileName if the new json structure is used
+  DESCRIPTION_NAME=$(jq -r '.beschrijving' "$CONFIG_NAME.json")
 
   echo "SPEC_NAME: $SPEC_NAME"
-  ## DESCRIPTION_NAME=$(jq -r '.beschrijving' "$CONFIG_NAME.json")
+  echo "DESCRIPTION_NAME: $DESCRIPTION_NAME"
 
   ## Normalizing spec name to be used as directory name
   NORMALIZED_SPEC_NAME="$(echo $SPEC_NAME | tr -c '[:alnum:]\n\r' '-' | tr -s '-' | tr '[:upper:]' '[:lower:]')"
@@ -35,5 +37,6 @@ do
 
   ## Copy the generated configuration file and description file to the nuxt directory
   cp "$CONFIG_NAME.json" "$NUXTDIR/$NORMALIZED_SPEC_NAME/configuration.json"
-  ## cp "descriptions/$DESCRIPTION_NAME" "$NUXTDIR/$NORMALIZED_SPEC_NAME/description.md"
+  cp "descriptions/$DESCRIPTION_NAME" "$NUXTDIR/$NORMALIZED_SPEC_NAME/description.md"
+  
 done < "$ROOTDIR/tmp-register.txt"
