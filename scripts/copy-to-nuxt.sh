bd69c95ffa306d168b62619c83de05e15c5ff0df
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2024-05-06 11:58:37 +0200

    Added extra echo statement to see the output of the config

--- scripts/copy-to-nuxt.sh
+++ scripts/copy-to-nuxt.sh
@@ -36,7 +36,10 @@ do
   mkdir -p "$NUXTDIR/$NORMALIZED_SPEC_NAME"
 
   # Store the repository name in the configuration file
-  jq --arg --arg REPOSITORY "$REPO_NAME" '. += {"repository": $REPOSITORY}' "$CONFIG_NAME.json" > "temp.json" && mv "temp.json" "$CONFIG_NAME.json"
+    jq --arg REPOSITORY "$REPO_NAME" '. + {"repository": $REPOSITORY}' "$CONFIG_NAME.json" > "temp.json" && mv "temp.json" "$CONFIG_NAME.json"
+
+
+  cat "$CONFIG_NAME.json"
 
   ## Copy the generated configuration file and description file to the nuxt directory
   cp "$CONFIG_NAME.json" "$NUXTDIR/$NORMALIZED_SPEC_NAME/configuration.json"
