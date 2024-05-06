819d068963289daa961f293eae92c95a2d05f84f
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2024-05-06 14:03:57 +0200

    Final version of the `sanitizeConfigurations` script

--- scripts/copy-to-nuxt.sh
+++ scripts/copy-to-nuxt.sh
@@ -38,8 +38,6 @@ do
   # Store the repository name in the configuration file
   jq --arg REPOSITORY "$REPO_NAME" '. + {"repository": $REPOSITORY}' "$CONFIG_NAME.json" > "temp.json" && mv "temp.json" "$CONFIG_NAME.json"
 
-  cat "$CONFIG_NAME.json"
-
   ## Copy the generated configuration file and description file to the nuxt directory
   cp "$CONFIG_NAME.json" "$NUXTDIR/$NORMALIZED_SPEC_NAME/configuration.json"
   cp "descriptions/$DESCRIPTION_NAME" "$NUXTDIR/$NORMALIZED_SPEC_NAME/description.md"
