4212f10795ba10f1a2d11183b5de179630971261
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2024-06-05 11:28:02 +0200

    Added`circleci/node` image and  fixes to `statistics.sh` script

--- scripts/statistics.sh
+++ scripts/statistics.sh
@@ -9,10 +9,13 @@ content=$(curl -s "https://raw.githubusercontent.com/Informatievlaanderen/data.v
 
 echo "$content"
 
+TOTAL_TERMS=$(echo "$content" | jq -r '.totalterms')
+
+echo "$TOTAL_TERMS"
+
 # Query certain keys and concatenate them into a JSON object
 json=$(echo "$content" | jq -r '{uniqueContributors: (.authors | tonumber) + (.editors | tonumber) + (.contributors | tonumber) + (.participants | tonumber)}')
 json=$(echo "$content" | jq -r '{: (totalorganisations | tonumber)}')
-
 echo "$json"
 
 # Store the result in statistics.json
