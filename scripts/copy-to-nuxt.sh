a3c9d2c119fe941a9e2e20b8b5ab5467705960b9
Bert Van Nuffelen <bert.vannuffelen@gmail.com>
2024-09-09 08:08:08 +0000

    stash changes

--- scripts/copy-to-nuxt.sh
+++ scripts/copy-to-nuxt.sh
@@ -4,6 +4,18 @@ ROOTDIR=$1
 REPODIR=$ROOTDIR/repositories
 NUXTDIR=$ROOTDIR/nuxt
 
+
+CONFIGDIR_DEFAULT=$( eval echo "${CIRCLE_WORKING_DIRECTORY}" )
+CONFIGDIR=${2:-$CONFIGDIR_DEFAULT/config}
+
+PRIMELANGUAGECONFIG=$(jq -r .primeLanguage ${CONFIGDIR}/config.json)
+GOALLANGUAGECONFIG=$(jq -r '.otherLanguages | @sh' ${CONFIGDIR}/config.json)
+GOALLANGUAGECONFIG=$(echo ${GOALLANGUAGECONFIG} | sed -e "s/'//g")
+
+PRIMELANGUAGE=${3-${PRIMELANGUAGECONFIG}}
+GOALLANGUAGE=${4-${GOALLANGUAGECONFIG}}
+
+
 mkdir -p "$NUXTDIR"
 
 echo ls -a
@@ -41,9 +53,12 @@ do
 
   ## Copy the generated configuration file and description file to the nuxt directory
 
+  for l in ${PRIMELANGUAGE} ; do
   node /app/node convert-config.js -i <input> -l <languagecode>'
 
   cp "$CONFIG_NAME.json" "$NUXTDIR/$NORMALIZED_SPEC_NAME/configuration.json"
   cp "descriptions/$DESCRIPTION_NAME" "$NUXTDIR/$NORMALIZED_SPEC_NAME/description.md"
+
+  done 
   
 done < "$ROOTDIR/tmp-register.txt"
