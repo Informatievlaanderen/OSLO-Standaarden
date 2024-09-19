7f121f4227f6594573af10c5c1490b1109e151d7
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2024-09-19 18:02:26 +0200

    WIP | migrate statistics to data.vlaanderen.be-statistics

--- scripts/statistics.sh
+++ scripts/statistics.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
 ROOTDIR=$1
-node /app/bin/contributors.js -f "$ROOTDIR/statistics_config.json" -o "$ROOTDIR/statistics.json"
 
+# Fetch the content from the URL
+content=$(curl -s "https://raw.githubusercontent.com/Informatievlaanderen/data.vlaanderen.be-statistics/production/aggr.stat")
+
+TOTAL_TERMS=$(echo "$content" | jq -r '.totalterms')
+
+echo "$TOTAL_TERMS"
+
+# Query certain keys and concatenate them into a JSON object
+uniqueContributors=$(echo "$content" | jq -r '(.authors | tonumber) + (.editors | tonumber) + (.contributors | tonumber) + (.participants | tonumber)')
+json=$(echo "$content" | jq -r --argjson uc "$uniqueContributors" '{uniqueContributors: $uc}')
+
+uniqueAffiliations=$(echo "$content" | jq -r '.totalorganisations | tonumber')
+json=$(echo "$json" | jq -r --argjson to "$uniqueAffiliations" '. + {uniqueAffiliations: $to}')
+
+# Read the existing content of statistics.json into a variable
+existingContent=$(cat "$ROOTDIR/statistics.json")
+
+# Add the new keys to the existing content
+updatedContent=$(echo "$existingContent" "$json" | jq -s add)
+
+# Write the updated content back to statistics.json
+echo "$updatedContent" >"$ROOTDIR/statistics.json"
