6c6635195730b15b6a367a9a714f2bd710198f4d
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2025-03-11 15:16:55 +0100

    Add an addition to also copy the `configuration.json` to the `PRIMELANGUAGE`

--- scripts/copy-to-nuxt.sh
+++ scripts/copy-to-nuxt.sh
@@ -5,8 +5,7 @@ REPODIR=$ROOTDIR/repositories
 NUXTDIR=$ROOTDIR/nuxt
 NUXTMEMORYDIR=$ROOTDIR/nuxtmemory
 
-
-CONFIGDIR_DEFAULT=$( eval echo "${CIRCLE_WORKING_DIRECTORY}" )
+CONFIGDIR_DEFAULT=$(eval echo "${CIRCLE_WORKING_DIRECTORY}")
 CONFIGDIR=${2:-$CONFIGDIR_DEFAULT/config}
 
 PRIMELANGUAGECONFIG=$(jq -r .primeLanguage ${CONFIGDIR}/config.json)
@@ -19,19 +18,24 @@ GOALLANGUAGE=${4-${GOALLANGUAGECONFIG}}
 translate_and_copy_config() {
   local CONFIGURATIONFILE=$1
 
-  md5sum ${CONFIGURATIONFILE} > ${CONFIGMD5SUM}
+  md5sum ${CONFIGURATIONFILE} >${CONFIGMD5SUM}
   for lang in ${GOALLANGUAGE}; do
-     node /app/autotranslate-config.js -i ${CONFIGURATIONFILE} -o "$CONFIG_NAME-multilang.json" -m ${PRIMELANGUAGE} -g $lang -s "$AZURETRANSLATIONKEY" >> "$ERROR_FILE"
+    node /app/autotranslate-config.js -i ${CONFIGURATIONFILE} -o "$CONFIG_NAME-multilang.json" -m ${PRIMELANGUAGE} -g $lang -s "$AZURETRANSLATIONKEY" >>"$ERROR_FILE"
   done
   cp "$CONFIG_NAME-multilang.json" "$NUXTDIR/$NORMALIZED_SPEC_NAME/multilang-configuration.json"
+
+  # Create directory for prime language and copy the original configuration
+  mkdir -p "$NUXTDIR/$NORMALIZED_SPEC_NAME/$PRIMELANGUAGE"
+  node /app/convert-config.js -i "$CONFIG_NAME-multilang.json" -l "$PRIMELANGUAGE" -o "$NUXTDIR/$NORMALIZED_SPEC_NAME/$PRIMELANGUAGE/configuration.json" >>"$ERROR_FILE"
+
   ## Convert config file to single language and copy the generated configuration file to the nuxt directory
   for lang in ${GOALLANGUAGE}; do
     mkdir -p "$NUXTDIR/$NORMALIZED_SPEC_NAME/$lang"
-    node /app/convert-config.js -i "$CONFIG_NAME-multilang.json" -l "$lang" -o "$NUXTDIR/$NORMALIZED_SPEC_NAME/$lang/configuration.json" >> "$ERROR_FILE"
+    node /app/convert-config.js -i "$CONFIG_NAME-multilang.json" -l "$lang" -o "$NUXTDIR/$NORMALIZED_SPEC_NAME/$lang/configuration.json" >>"$ERROR_FILE"
   done
 }
 
-filter_error_file() { 
+filter_error_file() {
   local ERROR_FILE=$1
   # Check if error file exists
   if [ -f "$ERROR_FILE" ]; then
@@ -51,8 +55,7 @@ mkdir -p "$NUXTDIR"
 echo ls -a
 
 ## Copy each configuration file and description file to content/standaarden folder
-while IFS= read -r line
-do
+while IFS= read -r line; do
   REPO_NAME=$(echo "$line" | cut -d ":" -f 1)
   CONFIG_NAME=$(echo "$line" | cut -d ":" -f 2 | cut -d "." -f 1)
 
@@ -60,7 +63,6 @@ do
   echo "CONFIG_NAME: $CONFIG_NAME"
 
   cd "$REPODIR/$REPO_NAME"
-
 
   ## this needs to change to .title if the new json structure is used
   SPEC_NAME=$(jq -r '.title' "$CONFIG_NAME.json")
@@ -85,9 +87,8 @@ do
   CONFIGURATIONFILE=${NUXTDIR}/${NORMALIZED_SPEC_NAME}/multilang-configuration.json
 
   # Store the repository name in the configuration file
-  jq --arg REPOSITORY "$REPO_NAME" '. + {"repository": $REPOSITORY}' "$CONFIG_NAME.json" > /tmp/temp.json 
+  jq --arg REPOSITORY "$REPO_NAME" '. + {"repository": $REPOSITORY}' "$CONFIG_NAME.json" >/tmp/temp.json
   mv /tmp/temp.json ${CONFIGURATIONFILE}
-
 
   CONFIGMD5SUM="$NUXTMEMORYNORMALIZED_SPEC_NAME/${PRIMELANGUAGE}/configuration.json.md5sum"
   mkdir -p ${NUXTMEMORYNORMALIZED_SPEC_NAME}/${PRIMELANGUAGE}
@@ -96,20 +97,20 @@ do
     CURSUM=$(md5sum ${CONFIGURATIONFILE})
     OLDSUM=$(cat "$CONFIGMD5SUM")
     if [ "$CURSUM" == "$OLDSUM" ]; then
-       echo "Use old configuration file"
-       for lang in ${GOALLANGUAGE}; do
-         cp "$NUXTMEMORYNORMALIZED_SPEC_NAME/$lang/configuration.json" "$NUXTDIR/$NORMALIZED_SPEC_NAME/$lang/configuration.json" || echo "Copy failed : cp $NUXTMEMORYNORMALIZED_SPEC_NAME/$lang/configuration.json $NUXTDIR/$NORMALIZED_SPEC_NAME/$lang/configuration.json" >> "$ERROR_FILE"
-       done
+      echo "Use old configuration file"
+      for lang in ${GOALLANGUAGE}; do
+        cp "$NUXTMEMORYNORMALIZED_SPEC_NAME/$lang/configuration.json" "$NUXTDIR/$NORMALIZED_SPEC_NAME/$lang/configuration.json" || echo "Copy failed : cp $NUXTMEMORYNORMALIZED_SPEC_NAME/$lang/configuration.json $NUXTDIR/$NORMALIZED_SPEC_NAME/$lang/configuration.json" >>"$ERROR_FILE"
+      done
     else
       translate_and_copy_config ${CONFIGURATIONFILE}
     fi
   else
     translate_and_copy_config ${CONFIGURATIONFILE}
-  fi  
+  fi
 
   # Check if the description file exists and didn't change in the memory
   DESCRIPTIONFILE=${NUXTDIR}/${NORMALIZED_SPEC_NAME}/description.md
-  cp "descriptions/$DESCRIPTION_NAME" "$DESCRIPTIONFILE" || echo "Copy failed : cp descriptions/$DESCRIPTION_NAME $DESCRIPTIONFILE" >> "$ERROR_FILE"
+  cp "descriptions/$DESCRIPTION_NAME" "$DESCRIPTIONFILE" || echo "Copy failed : cp descriptions/$DESCRIPTION_NAME $DESCRIPTIONFILE" >>"$ERROR_FILE"
   MD5SUMFILE="$NUXTMEMORYNORMALIZED_SPEC_NAME/${PRIMELANGUAGE}/description.md.md5sum"
   # Check if file is in memory
   if [ -f "$NUXTMEMORYNORMALIZED_SPEC_NAME/${PRIMELANGUAGE}/description.md" ]; then
@@ -117,21 +118,21 @@ do
     CURSUM=$(md5sum "$DESCRIPTIONFILE")
     OLDSUM=$(cat "$MD5SUMFILE")
     if [ "$CURSUM" == "$OLDSUM" ]; then
-       echo "Use old description file"
-       for lang in ${GOALLANGUAGE}; do
-         cp "$NUXTMEMORYNORMALIZED_SPEC_NAME/$lang/description.md" "$NUXTDIR/$NORMALIZED_SPEC_NAME/$lang/description.md" || echo "Copy failed : cp $NUXTMEMORYNORMALIZED_SPEC_NAME/$lang/description.md $NUXTDIR/$NORMALIZED_SPEC_NAME/$lang/description.md" >> "$ERROR_FILE"
-       done
+      echo "Use old description file"
+      for lang in ${GOALLANGUAGE}; do
+        cp "$NUXTMEMORYNORMALIZED_SPEC_NAME/$lang/description.md" "$NUXTDIR/$NORMALIZED_SPEC_NAME/$lang/description.md" || echo "Copy failed : cp $NUXTMEMORYNORMALIZED_SPEC_NAME/$lang/description.md $NUXTDIR/$NORMALIZED_SPEC_NAME/$lang/description.md" >>"$ERROR_FILE"
+      done
     else
-      md5sum "$DESCRIPTIONFILE" > "$MD5SUMFILE"
-       for lang in ${GOALLANGUAGE}; do
-          node /app/autotranslate-md.js -i "$DESCRIPTIONFILE" -m ${PRIMELANGUAGE} -g ${lang} -s "$AZURETRANSLATIONKEY" >> "$ERROR_FILE"
-     done
+      md5sum "$DESCRIPTIONFILE" >"$MD5SUMFILE"
+      for lang in ${GOALLANGUAGE}; do
+        node /app/autotranslate-md.js -i "$DESCRIPTIONFILE" -m ${PRIMELANGUAGE} -g ${lang} -s "$AZURETRANSLATIONKEY" >>"$ERROR_FILE"
+      done
     fi
   else
-    md5sum "$DESCRIPTIONFILE" > "$MD5SUMFILE"
-       for lang in ${GOALLANGUAGE}; do
-    node /app/autotranslate-md.js -i "$DESCRIPTIONFILE" -m ${PRIMELANGUAGE} -g $lang  -s "$AZURETRANSLATIONKEY" >> "$ERROR_FILE"
-done
+    md5sum "$DESCRIPTIONFILE" >"$MD5SUMFILE"
+    for lang in ${GOALLANGUAGE}; do
+      node /app/autotranslate-md.js -i "$DESCRIPTIONFILE" -m ${PRIMELANGUAGE} -g $lang -s "$AZURETRANSLATIONKEY" >>"$ERROR_FILE"
+    done
   fi
 
   # Filter the error file
@@ -140,7 +141,6 @@ done
   rm "$NUXTDIR/$NORMALIZED_SPEC_NAME/description.md"
 
   # Copy the translations to the memory
-  cp -r "$NUXTDIR/$NORMALIZED_SPEC_NAME/"* "$NUXTMEMORYNORMALIZED_SPEC_NAME" || echo "Copy failed : cp -r $NUXTDIR/$NORMALIZED_SPEC_NAME/* $NUXTMEMORYNORMALIZED_SPEC_NAME" >> "$ERROR_FILE"
+  cp -r "$NUXTDIR/$NORMALIZED_SPEC_NAME/"* "$NUXTMEMORYNORMALIZED_SPEC_NAME" || echo "Copy failed : cp -r $NUXTDIR/$NORMALIZED_SPEC_NAME/* $NUXTMEMORYNORMALIZED_SPEC_NAME" >>"$ERROR_FILE"
 
-  
-done < "$ROOTDIR/tmp-register.txt"
+done <"$ROOTDIR/tmp-register.txt"
